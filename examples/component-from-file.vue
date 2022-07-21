03daaaa83dc4cc5edadd24dfc4c95976173ed4c6
Julian Rüth <julian.rueth@fsfe.org>
2022-07-21 03:09:23 +0300

    Add examples

---
+++ examples/component-from-file.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="styled">Component from File</div>
+</template>
+<style>
+.styled {
+  color: cornflowerblue;
+}
+</style>
---
+++ examples/component-with-python-script.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>Component whose &lt;script&gt; imports a Python module</div>
+</template>
+<script>
+import { component } from "./component_with_python_script.py";
+export default component;
+</script>
