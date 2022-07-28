65f5bd13a81842deb096c14011707b5b7e6e5f01
Julian Rüth <julian.rueth@fsfe.org>
2022-07-28 12:10:10 +0530

    bumped version to 0.0.4

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ cmdclass['jsdeps'] = combine_commands(
 
 setup_args = dict(
     name='ipymuvue',
-    version="0.0.3",
+    version="0.0.4",
     description='Reactive Jupyter Widgets',
     long_description='Reactive Jupyter Widgets',
     include_package_data=True,
