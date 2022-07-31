94730f97245aea7e09fef2cb70e3e0c910b03686
Julian Rüth <julian.rueth@fsfe.org>
2022-07-31 09:07:40 +0530

    bumped version to 0.1.0

--- ipymuvue/version.py
+++ ipymuvue/version.py
@@ -15,5 +15,5 @@
 # along with ipymuvue. If not, see <https://www.gnu.org/licenses/>.
 # ******************************************************************************
 
-version_info = (0, 0, 7)
-__version__ = "0.0.7"
+version_info = (0, 1, 0)
+__version__ = "0.1.0"
