412ff7e703fea4aa0d4f63cdf4419a7ead1ecb6e
Julian Rüth <julian.rueth@fsfe.org>
2022-07-31 07:30:39 +0530

    Fix typo in filename

--- examples/demo-files/component_with_python_script.py
+++ examples/demo-files/component_with_python_script.py
@@ -5,6 +5,6 @@ from subcomponent_pure_python import component as subcomponent_pure_python
 component = define_component(
     components=dict(
         subcomponentPurePython=subcomponent_pure_python,
-        subcomponentWithPythonScript=open("subcomponent-with-python-script.vue"),
+        subcomponentWithPythonScript=open("SubcomponentWithPythonScript.vue"),
     ),
 )
